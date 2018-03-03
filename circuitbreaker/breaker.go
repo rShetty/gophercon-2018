2d3a17bb5ae15111fe002bd657df92f62bb28d3a
Rajeev N B <rajeev.bharshetty@go-jek.com>
2018-03-03 21:30:56 +0530

    add circuit breaker demo

---
+++ circuitbreaker/breaker.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/bcicen/grmon"
+	"github.com/gojektech/heimdall"
+)
+
+func main() {
+	http.HandleFunc("/hello", http.HandlerFunc(hello))
+	log.Print("Started server on port :8004")
+	http.ListenAndServe(":8004", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Print("Making request to slow server on port:", 9999)
+
+	fallbackFn := func(err error) error {
+		// Handle failure
+		return err
+	}
+
+	timeoutInMillis := 10
+	timeout := 10 * time.Millisecond
+
+	hystrixConfig := heimdall.NewHystrixConfig("MyCommand", heimdall.HystrixCommandConfig{
+		Timeout:                timeoutInMillis,
+		MaxConcurrentRequests:  100,
+		ErrorPercentThreshold:  25,
+		SleepWindow:            10,
+		RequestVolumeThreshold: 10,
+		FallbackFunc:           fallbackFn,
+	})
+
+	httpClient := heimdall.NewHystrixHTTPClient(timeout, hystrixConfig)
+
+	_, err := httpClient.Get("http://localhost:9999/drivers", http.Header{})
+	if err != nil {
+		log.Print("failed with error: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
